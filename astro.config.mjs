8520cb7280a2af4ecccc2f5fda67562dec5159dd
Trohan0x00 <trietnguyen1892@gmail.com>
2025-09-04 23:45:50 +0700

    Init Mizuki Astro blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://mizuki.mysqil.com/",
+	site: "https://trohan0x00.github.io/",
 
 	base: "/",
 	trailingSlash: "always",
